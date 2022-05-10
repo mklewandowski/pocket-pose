eb58b4c4fbaea06da8104278454873fb197bcf30
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-05-10 13:21:17 -0700

    Add intro and about panels

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject HUDMainButtons;
+    [SerializeField]
+    GameObject HUDAbout;
+
+    AudioSource audioSource;
+    [SerializeField]
+    AudioClip MenuSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SelectAboutButton()
+    {
+        audioSource.PlayOneShot(MenuSound, 1f);
+        HUDMainButtons.GetComponent<MoveNormal>().MoveRight();
+        HUDAbout.GetComponent<MoveNormal>().MoveLeft();
+    }
+
+    public void SelectAboutBackButton()
+    {
+        audioSource.PlayOneShot(MenuSound, 1f);
+        HUDMainButtons.GetComponent<MoveNormal>().MoveLeft();
+        HUDAbout.GetComponent<MoveNormal>().MoveRight();
     }
 }
